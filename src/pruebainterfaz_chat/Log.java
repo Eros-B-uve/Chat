be2b7aecb4a0ace00921f7588468e30fc432afde
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2023-03-29 20:11:27 -0600

    Se modifico la clase Log

--- src/pruebainterfaz_chat/Log.java
+++ src/pruebainterfaz_chat/Log.java
@@ -1,7 +1,17 @@
 package pruebainterfaz_chat;
 
-public class Log {
+import javax.swing.JFrame;
+
+public class Log extends JFrame{
+
+    public Log () {
+        this.setLocationRelativeTo(null);
+        this.setTitle("Interfaz Chat");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+    }
     public static void main(String args []) {
-        
+        Log log = new Log();
+        log.setVisible(true);
     }
 }
