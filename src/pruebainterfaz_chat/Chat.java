ef5ec0baaacc11a58d3507cadc36e496071f54db
Erick <comida@gmail.com>
2023-03-30 13:10:04 -0600

    Se cambio la configuracion de Chat

--- src/pruebainterfaz_chat/Chat.java
+++ src/pruebainterfaz_chat/Chat.java
@@ -15,7 +15,6 @@ public class Chat extends JFrame{
     Usuario u;
 
     public Chat(Usuario u) {
-        this.u = u;
         this.setTitle("Interfaz Chat");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(500, 500);
@@ -23,33 +22,33 @@ public class Chat extends JFrame{
         this.setResizable(false);
         this.setLayout(null);
 
+        this.u = u;
+
         componentes();
     }
 
     private void componentes() {
         JPanel panel = new JPanel(null);
+        panel.setBounds(0,0,500, 500);
 
         chatBox = new JTextArea();
         chatBox.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(chatBox);
-        scrollPane.setBounds(0,0,500,500);
-        panel.add(scrollPane);
+        scrollPane.setBounds(0,0,300, 430);
 
-        JPanel inputPanel = new JPanel(null);
         inputField = new JTextField();
-        inputField.setBounds(0,0,400,30);
-        JButton enviar = new JButton("Enviar");
-        enviar.setBackground(u.getColor());
-        enviar.addActionListener(new SendButtonListener());
-        enviar.setBounds(400, 0, 100, 30);
-        inputPanel.add(inputField);
-        inputPanel.add(enviar);
-        
-        panel.setBounds(0,0,500,400);
-        inputPanel.setBounds(0,400,500,100);
+        inputField.setBounds(1, 430,400, 30);
 
+        JButton enviar = new JButton("Enviar");
+        //enviar.setBackground(u.getColor());
+        enviar.addActionListener(new SendButtonListener());
+        enviar.setBounds(400, 430, 100, 30);
+        
+
+        panel.add(scrollPane);
+        panel.add(inputField);
+        panel.add(enviar);
         add(panel);
-        add(inputPanel);
     }
 
     private class SendButtonListener implements ActionListener {
