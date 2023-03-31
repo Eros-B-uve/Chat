33ea197c3e6dc958cace99e93d4e0373d2abb773
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2023-03-30 20:33:46 -0600

    Se esta tratando de implementar el cambio de color en el usuario

--- src/pruebainterfaz_chat/Chat.java
+++ src/pruebainterfaz_chat/Chat.java
@@ -1,14 +1,14 @@
 package pruebainterfaz_chat;
-import javax.swing.*;
 
-import java.awt.Font;
-//import java.awt.*;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import javax.swing.text.*;
 
 //https://stackoverflow.com/questions/9650992/how-to-change-text-color-in-the-jtextarea
-public class Chat extends JFrame{
+public class Chat extends JFrame {
 
-    private JTextArea chatBox;
+    private JTextPane chatBox;
     private JTextField inputField;
     Usuario u;
 
@@ -27,33 +27,49 @@ public class Chat extends JFrame{
 
     private void componentes() {
         JPanel panel = new JPanel(null);
-        panel.setBounds(0,0,500, 500);
+        panel.setBounds(0, 0, 500, 500);
 
-        chatBox = new JTextArea();
+        chatBox = new JTextPane();
         chatBox.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(chatBox);
-        scrollPane.setBounds(0,0, 500, 430);
+        chatBox.setBounds(0, 0, 100, 100);
+        JScrollPane scrollPane = new JScrollPane(chatBox, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBounds(0, 0, 490, 430);
 
         inputField = new JTextField();
-        inputField.setBounds(1, 430,400, 30);
+        inputField.setBounds(1, 430, 400, 30);
         inputField.setFont(new Font("Arial", Font.PLAIN, 12));
 
         JButton enviar = new JButton("Enviar");
         enviar.addActionListener(new SendButtonListener());
         enviar.setBounds(400, 430, 100, 30);
-        
-        panel.add(scrollPane);
+
+            appendToPane(chatBox, "Hola\n", Color.red);
+            appendToPane(chatBox, "Hola\n", Color.red);
+        panel.add(chatBox);
         panel.add(inputField);
         panel.add(enviar);
         add(panel);
     }
 
+    private void appendToPane(JTextPane tp, String msg, Color c) {
+        StyleContext sc = StyleContext.getDefaultStyleContext();
+        AttributeSet aset = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, c);
+
+        aset = sc.addAttribute(aset, StyleConstants.FontFamily, "Lucida Console");
+        aset = sc.addAttribute(aset, StyleConstants.Alignment, StyleConstants.ALIGN_JUSTIFIED);
+
+        int len = tp.getDocument().getLength();
+        tp.setCaretPosition(len);
+        tp.setCharacterAttributes(aset, false);
+        tp.replaceSelection(msg);
+    }
+
     private class SendButtonListener implements ActionListener {
+
         public void actionPerformed(ActionEvent e) {
-            chatBox.append(u.getNombre() + ": " + inputField.getText() + "\n");
+            appendToPane(chatBox, "Hola\n", Color.red);
             inputField.setText("");
         }
     }
-
 
 }
